174c8f6dfca09543441a2e28fbbcdb4e94297b48
petermalak <pmalak14@gmail.com>
2023-09-17 22:29:52 +0300

    test

--- config/logging.php
+++ config/logging.php
@@ -52,10 +52,14 @@ return [
     */
 
     'channels' => [
+        // 'stack' => [
+        //     'driver' => 'stack',
+        //     'channels' => ['single'],
+        //     'ignore_exceptions' => false,
+        // ],
         'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
-            'ignore_exceptions' => false,
+            'driver' => 'stderr',
+            'level' => 'debug',
         ],
 
         'single' => [
