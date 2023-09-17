7f28500499011a015da0db42af45d9abaf4879ea
petermalak <pmalak14@gmail.com>
2023-09-17 23:09:51 +0300

    fix

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{route("dashboard")}}" class="nav-link {{areActiveRoutes(['dashboard'])}}">
+                    <a href="{{route("dashboard")}}" class="nav-link ">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>
