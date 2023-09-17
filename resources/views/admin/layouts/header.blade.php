0729dbf2d1e476fde2b01b9c74b5342ffa159bff
petermalak <pmalak14@gmail.com>
2023-09-17 22:54:55 +0300

    fix

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield("title", title())</title>
+    {{-- <title>@yield("title", title())</title> --}}
 
     <meta name="csrf-token" content="{{csrf_token()}}">
     <link rel="icon" href="/styles/admin/dist/img/favicon.png"/>
