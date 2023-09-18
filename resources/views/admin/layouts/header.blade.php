91a45515f3fc812a390b916d86d669062ae630c6
petermalak <pmalak14@gmail.com>
2023-09-18 13:28:43 +0300

    adding css

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -12,23 +12,23 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/styles/admin/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="/styles/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="/styles/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="/css/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="/styles/admin/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="/css/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/styles/admin/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="/styles/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="/styles/admin/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="/css/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="/styles/admin/plugins/summernote/summernote-bs4.min.css">
+    {{-- <link rel="stylesheet" href="/styles/admin/plugins/summernote/summernote-bs4.min.css"> --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>--}}
@@ -39,7 +39,7 @@
           href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
 
     <!-- Custom style -->
-    <link rel="stylesheet" href="/styles/admin/dist/css/custom.css">
+    <link rel="stylesheet" href="/css/custom.css">
     @stack("styles")
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
